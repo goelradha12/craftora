a2f42eddada7fa70d0a1db0d069db77480b1ff0e
Radha . <radha@adobe.com>
2026-05-23 17:48:05 +0530

    Header made responsive, added 'skip to content' button

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 
-const nav_btn = document.querySelector(".js_close_btn");
+const nav_close_btn = document.querySelector(".js_close_btn");
 const nav_open_btn = document.querySelector(".js_open_btn");
-nav_btn.addEventListener("click",()=>{
-  document.querySelector(".js_nav_list").style.transform = "translateX(100%)";
-})
-nav_open_btn.addEventListener("click",()=>{
-  document.querySelector(".js_nav_list").style.transform = "translateX(0)";       
-})
+const nav_list = document.querySelector(".js_nav_list");
+nav_open_btn.addEventListener("click", () => {
+  nav_list.classList.add("active");
+
+  nav_open_btn.style.display = "none";
+  nav_close_btn.style.display = "block";
+});
+
+nav_close_btn.addEventListener("click", () => {
+  nav_list.classList.remove("active");
+
+  nav_open_btn.style.display = "block";
+  nav_close_btn.style.display = "none";
+});
+
+const dropdownToggle = document.querySelector(".dropdown--toggle");
+const dropdown = document.querySelector(".dropdown");
+
+dropdownToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  dropdown.classList.toggle("active");
+});
