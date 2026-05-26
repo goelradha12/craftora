1b7d641d5a05f68490d365fbd9b00d979ad5c04b
Radha . <radha@adobe.com>
2026-05-26 10:19:21 +0530

    Added hero section in home page

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,55 @@ dropdownToggle.addEventListener("click", (e) => {
   e.preventDefault();
   dropdown.classList.toggle("active");
 });
+
+
+// ------------ Hero - Reviews ---------------
+
+const reviewsTrack = document.querySelector(".js_reviews_track");
+
+let scrollAmount = 0;
+
+async function loadReviews() {
+
+    const response = await fetch("../content/reviews.json");
+
+    const reviews = await response.json();
+
+    // duplicate reviews for infinite effect
+    const duplicatedReviews = [...reviews, ...reviews];
+
+    duplicatedReviews.forEach((item) => {
+
+        const card = document.createElement("div");
+
+        card.classList.add("review");
+
+        card.innerHTML = `
+            <p>"${item.review}"</p>
+            <span>— ${item.author}</span>
+        `;
+
+        reviewsTrack.appendChild(card);
+
+    });
+
+}
+
+function autoScroll() {
+
+    scrollAmount += 0.5;
+
+    if (scrollAmount >= reviewsTrack.scrollWidth / 2) {
+        scrollAmount = 0;
+    }
+
+    reviewsTrack.style.transform =
+        `translateX(-${scrollAmount}px)`;
+
+    requestAnimationFrame(autoScroll);
+
+}
+
+loadReviews().then(() => {
+    autoScroll();
+});
